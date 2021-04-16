431fde4033bb62b4e0a823cac1f2d9b0772a9c2c
Loganathan M <Loganathan_M3@Dellteam.com>
2021-04-16 20:38:31 +0530

    Extract routes and their respective resource urls

---
+++ Enigma1337/Common/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Enigma1337
+{
+     public static class Constants
+    {
+        public const string RouteRegex = "//a[@href]";
+        public const string LinkRegex = "//link[@href]|//script[@src]";
+        public const string Website = "https://tretton37.com/";
+    }
+}
---
+++ Enigma1337/ResoureUrlProvider.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Enigma1337
+{
+    public static class ResoureUrlProvider
+    {
+        /// <summary>
+        /// Get the website routes
+        /// </summary>
+        /// <param></param>
+        /// <remarks>
+        /// Extract and format all the routes used in the website
+        /// </remarks>
+        /// <returns> List of website routes </returns>
+        public static List<string> GetWebsiteRoutes()
+        {
+            var partialRoutes = HtmlLinkExtractor.ExtractLinksFromWebsite(Constants.RouteRegex);
+            var formattedRoutes = LinkFormatter.Format(partialRoutes);
+            var routes = formattedRoutes.FindAll(x => x.Contains(Constants.Website)).Distinct(StringComparer.InvariantCultureIgnoreCase).ToList();
+            return routes;
+        }
+
+        /// <summary>
+        /// Get the resource urls
+        /// </summary>
+        /// <param></param>
+        /// <remarks>
+        /// Extract and format the resource based urls from a particular webpage
+        /// </remarks>
+        /// <returns> List of formatted urls </returns>
+        public static List<string> GetWebpageUrls( string websiteRoute)
+        {
+            List<string> unformattedUrls = HtmlLinkExtractor.ExtractLinksFromWebsite(Constants.LinkRegex, websiteRoute);
+            List<string> formattedUrls = LinkFormatter.Format(unformattedUrls);
+            return formattedUrls;
+        }
+    }
+}
