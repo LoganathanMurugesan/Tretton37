c808f6b064a1b6a724ddfcdfac0f3b0808865330
Loganathan M <Loganathan_M3@Dellteam.com>
2021-04-16 12:05:34 +0530

    Set up the local directory finder and integrating it with the downloader

--- Enigma1337/ResourceDownloader.cs
+++ Enigma1337/ResourceDownloader.cs
@@ -9,15 +9,60 @@ namespace Enigma1337
     public static class ResourceDownloader
     {
 
-        //Testing the resource downloading.
-        //To do - find and download the file to its respective directory.
+        /// <summary>
+        /// Downloads the resources from website.
+        /// </summary>
+        /// <param name="formattedUrls"> List of formatted urls</param>
+        /// <remarks>
+        /// Download the resources based on the supplied formatted urls
+        /// and saves to their respective local directories.
+        /// </remarks>
+        /// <returns> Returns nothing</returns>
         public static void Download(List<string> formattedUrls)
         {
             Console.WriteLine("Started downloading");
             WebClient webClient = new WebClient();
-            webClient.DownloadFile("https://tretton37.com//assets/js/video.js", "C:\\Users\\Loganathan_M3\\Downloads\\assets\\testfile");
+            foreach (var formattedUrl in formattedUrls)
+            {
+                var filename = formattedUrl.Split('/').Last().Replace('?', '_');
+                webClient.DownloadFile(formattedUrl, DirectoryFinder(formattedUrl, filename));
+            }
             Console.WriteLine("Completed downloading");
-            Console.ReadLine();
+        }
+
+
+        private static string DirectoryFinder(string formattedUrl, string fileType)
+        {
+            var temp1 = formattedUrl.Split('/').Last();
+            var temp2 = formattedUrl.Replace('/' + temp1, "");
+            var remoteFoldername = temp2.Split('/').Last();
+
+            string targetDirectory;
+            switch (remoteFoldername)
+            {
+                case "css":
+                    targetDirectory = $"C:\\Users\\Loganathan_M3\\Downloads\\assets\\css\\{fileType}";
+                    break;
+                case "fonts":
+                    targetDirectory = $"C:\\Users\\Loganathan_M3\\Downloads\\assets\\fonts\\{fileType}";
+                    break;
+                case "i":
+                    targetDirectory = $"C:\\Users\\Loganathan_M3\\Downloads\\assets\\i\\{fileType}";
+                    break;
+                case "js":
+                    targetDirectory = $"C:\\Users\\Loganathan_M3\\Downloads\\assets\\js\\{fileType}";
+                    break;
+                default:
+                    if (fileType.Contains(".css"))
+                        targetDirectory = $"C:\\Users\\Loganathan_M3\\Downloads\\assets\\css\\{fileType}";
+                    if (fileType.Contains(".js"))
+                        targetDirectory = $"C:\\Users\\Loganathan_M3\\Downloads\\assets\\js\\{fileType}";
+                    targetDirectory = $"C:\\Users\\Loganathan_M3\\Downloads\\assets\\js\\{fileType}";
+                    break;
+            }
+
+
+            return targetDirectory;
         }
     }
 }
